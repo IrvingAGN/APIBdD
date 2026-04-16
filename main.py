b083879896c94e29a8c1632a3f98f3144f962275
IrvingAGN <irving1326yt@gmail.com>
2026-04-16 14:05:54 -0600

    Agregar funcionalidad para registrar cuentas y seleccionar saldo, ajustando consultas y parámetros.

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ class Tutor(BaseModel):
     correo: str
     password: str
 
+class Cuenta(BaseModel):
+    id_nino: int
+    mini_coins: int
+    saldo: float
+    
 # Rutas para las consultas
 
 @app.post("/registrar_nino")
@@ -29,8 +34,8 @@ def registrar_nino(nino: Nino):
         conn = psycopg2.connect(DB_URL)
         cur = conn.cursor()
         
-        query = "INSERT INTO nino (nombre, edad) VALUES (%s, %s) RETURNING id_nino;"
-        cur.execute(query, (nino.nombre, nino.edad))
+        query = "INSERT INTO nino (nombre, edad,  id_tutor) VALUES (%s, %s, %s) RETURNING id_nino;"
+        cur.execute(query, (nino.nombre, nino.edad, nino.id_tutor))
         
         nuevo_id = cur.fetchone()[0]
         conn.commit()
@@ -41,7 +46,6 @@ def registrar_nino(nino: Nino):
     except Exception as e:
         return {"error": str(e)}
     
-#hola?
 @app.post("/registrar_tutor")
 def registrar_tutor(tutor: Tutor):
     try:
@@ -60,27 +64,48 @@ def registrar_tutor(tutor: Tutor):
     except Exception as e:
         return {"error": str(e)}
     
-
-#QUERYS DE LA PANTALLA PRINCIPAL
-@app.get("/seleccionar_saldo/{id_nino}")
-def seleccionar_saldo(id_nino: int):
+@app.post("/registrar_cuenta")
+def registrar_cuenta(cuenta: Cuenta):
     try:
         conn = psycopg2.connect(DB_URL)
         cur = conn.cursor()
         
-        query = "SELECT saldo FROM nino WHERE id_nino = %s;"
-        cur.execute(query, (id_nino))
+        query = "INSERT INTO cuenta (id_nino, mini_coins, saldo) VALUES (%s, %s, %s) RETURNING id_cuenta;"
+        cur.execute(query, (cuenta.id_nino, cuenta.mini_coins, cuenta.saldo))
         
-        resultado = cur.fetchone()[0]
+        nuevo_id = cur.fetchone()[0]
         conn.commit()
         
         cur.close()
         conn.close()
+        return {"mensaje": "Cuenta registrada con éxito", "id": nuevo_id}
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+#QUERYS DE LA PANTALLA PRINCIPAL
+@app.get("/obtener_cuenta/{id_cuenta}")
+def seleccionar_saldo(id_cuenta: int):
+    try:
+        conn = psycopg2.connect(DB_URL)
+        cur = conn.cursor()
+        
+        query = "SELECT id_nino, mini_coins, saldo FROM cuenta WHERE id_cuenta = %s;"
+        cur.execute(query, (id_cuenta))
+        resultado = cur.fetchone()
+            
+        cur.close()
+        conn.close()
 
         if resultado:
-            return {"saldo": float(resultado[0])}
+            # resultado[0] es id_nino, resultado[1] es mini_coins, resultado[2] es saldo
+            return {
+                "id_nino": resultado[0],
+                "mini_coins": resultado[1],
+                "saldo": float(resultado[2])
+            }
         else:
-            return {"error": "No se encontró una cuenta para este niño"}
+            return {"error": "No se encontró la cuenta"}
             
     except Exception as e:
         return {"error": str(e)}
