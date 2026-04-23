f00574d12ec6da441dc6b1a8748d82f8fa74eac1
SkullCandy <pinedalopezdulce@gmail.com>
2026-04-23 16:22:47 -0600

    Query de la tabla de niño

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 from fastapi import FastAPI
 import psycopg2
 from pydantic import BaseModel
@@ -6,14 +8,14 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # External URL de DB
-DB_URL = "postgresql://bdd_bancomini2_user:pVoA1Eyf203A8OJyjAiEODdJzavDAKbQ@dpg-d7l6oj5ckfvc73a2ju6g-a.oregon-postgres.render.com/bdd_bancomini2"
+DB_URL = "postgresql://api_bdd_bancomini_user:xyDekXdHvLwtnnz5ya1Qtkw8YonktGBh@dpg-d6u4vp7kijhs73fgumug-a.oregon-postgres.render.com/api_bdd_bancomini"
 
 # Modelo de datos para recibir desde Android
 class Nino(BaseModel):
     nombre: str
     edad: int
-    id_tutor: int
-
+    correo: str
+    password: str
 
 class Tutor(BaseModel):
     nombre: str
@@ -21,11 +23,6 @@ class Tutor(BaseModel):
     correo: str
     password: str
 
-class Cuenta(BaseModel):
-    id_nino: int
-    mini_coins: int
-    saldo: float
-    
 # Rutas para las consultas
 
 @app.post("/registrar_nino")
@@ -34,8 +31,8 @@ def registrar_nino(nino: Nino):
         conn = psycopg2.connect(DB_URL)
         cur = conn.cursor()
         
-        query = "INSERT INTO nino (nombre, edad,  id_tutor) VALUES (%s, %s, %s) RETURNING id_nino;"
-        cur.execute(query, (nino.nombre, nino.edad, nino.id_tutor))
+        query = "INSERT INTO nino (nombre, edad) VALUES (%s, %s) RETURNING id_nino;"
+        cur.execute(query, (nino.nombre, nino.edad))
         
         nuevo_id = cur.fetchone()[0]
         conn.commit()
@@ -46,6 +43,7 @@ def registrar_nino(nino: Nino):
     except Exception as e:
         return {"error": str(e)}
     
+#hola?
 @app.post("/registrar_tutor")
 def registrar_tutor(tutor: Tutor):
     try:
@@ -64,48 +62,56 @@ def registrar_tutor(tutor: Tutor):
     except Exception as e:
         return {"error": str(e)}
     
-@app.post("/registrar_cuenta")
-def registrar_cuenta(cuenta: Cuenta):
+
+#QUERYS DE LA PANTALLA PRINCIPAL
+@app.get("/seleccionar_saldo/{id_nino}")
+def seleccionar_saldo(id_nino: int):
     try:
         conn = psycopg2.connect(DB_URL)
         cur = conn.cursor()
         
-        query = "INSERT INTO cuenta (id_nino, mini_coins, saldo) VALUES (%s, %s, %s) RETURNING id_cuenta;"
-        cur.execute(query, (cuenta.id_nino, cuenta.mini_coins, cuenta.saldo))
+        query = "SELECT saldo FROM nino WHERE id_nino = %s;"
+        cur.execute(query, (id_nino))
         
-        nuevo_id = cur.fetchone()[0]
+        resultado = cur.fetchone()[0]
         conn.commit()
         
         cur.close()
         conn.close()
-        return {"mensaje": "Cuenta registrada con éxito", "id": nuevo_id}
+
+        if resultado:
+            return {"saldo": float(resultado[0])}
+        else:
+            return {"error": "No se encontró una cuenta para este niño"}
+            
     except Exception as e:
         return {"error": str(e)}
     
-
-#QUERYS DE LA PANTALLA PRINCIPAL
-@app.get("/obtener_cuenta/{id_cuenta}")
-def seleccionar_saldo(id_cuenta: int):
+@app.get("/seleccionar_info_nino/{id_nino}")
+def seleccionar_info_nino(id_nino: int):
     try:
         conn = psycopg2.connect(DB_URL)
         cur = conn.cursor()
         
-        query = "SELECT id_nino, mini_coins, saldo FROM cuenta WHERE id_cuenta = %s;"
-        cur.execute(query, (id_cuenta))
-        resultado = cur.fetchone()
-            
+        query = "SELECT * FROM nino WHERE id_nino = %s;"
+        cur.execute(query, (id_nino))
+        
+        resultado = cur.fetchone()[0]
+        conn.commit()
+        
         cur.close()
         conn.close()
 
         if resultado:
-            # resultado[0] es id_nino, resultado[1] es mini_coins, resultado[2] es saldo
             return {
-                "id_nino": resultado[0],
-                "mini_coins": resultado[1],
-                "saldo": float(resultado[2])
+                "id_tutor": int(resultado[0]),
+                "nombre": str(resultado[1]),
+                "edad": int(resultado[2])
             }
+            
+
         else:
-            return {"error": "No se encontró la cuenta"}
+            return {"error": "No se encontró una cuenta para este niño"}
             
     except Exception as e:
         return {"error": str(e)}
